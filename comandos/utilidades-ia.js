ba4cf9a35318f294dd99c1affa40f64db5e41a66
Yanniel <yannielmedrano1@gmail.com>
2026-04-07 20:30:10 -0400

    Update utilidades-ia.js

--- comandos/utilidades-ia.js
+++ comandos/utilidades-ia.js
@@ -1,3 +1,7 @@
+/* Código creado por Yanniel y ABRAHAN-M
+por favor y no quites los créditos.
+https://github.com/yannielmedrano1-sys
+*/
 import axios from 'axios';
 
 // Memoria temporal de sesiones (se mantiene en RAM)
